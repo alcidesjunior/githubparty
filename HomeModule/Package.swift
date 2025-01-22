0fd184fb490d686a47a2b4e37587c55861b068c4
Alcides Junior <alcidesjunior.infor@gmail.com>
2025-01-22 01:30:45 -0300

    add modules using spm

---
+++ HomeModule/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HomeModule",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "HomeModule",
+            targets: ["HomeModule"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "HomeModule"),
+        .testTarget(
+            name: "HomeModuleTests",
+            dependencies: ["HomeModule"]),
+    ]
+)
